577899e70a2f9f4592305a949d4dcae703c0be2b
Claneercoles <pedro.ivoc@sempreceub.com>
2025-04-03 10:17:31 -0300

    AVA2-IMC – Análise do IMC

---
+++ AVA2-IMC – Análise do IMC/TesteCalculoimc.java
@@ -0,0 +1,7 @@
+public class TesteCalculoimc{
+
+    public static void main(String[] args){
+        Calculoimc pessoa = new Calculoimc("Roberto", 1.72, 78.2, 0);
+        System.out.println (pessoa);
+    }
+}
