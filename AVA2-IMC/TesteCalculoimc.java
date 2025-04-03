34676134dcdded0fbecfa6adf0fef907ed25e14a
Claneercoles <pedro.ivoc@sempreceub.com>
2025-04-03 10:17:05 -0300

    Delete AVA2-IMC directory

--- AVA2-IMC/TesteCalculoimc.java
+++
@@ -1,7 +0,0 @@
-public class TesteCalculoimc{
-
-    public static void main(String[] args){
-        Calculoimc pessoa = new Calculoimc("Roberto", 1.72, 78.2, 0);
-        System.out.println (pessoa);
-    }
-}
